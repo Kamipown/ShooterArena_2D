256a4f4b50058f35c9922adc856d92dc0196fb4b
Paul Delobbel <paul.delobbel@gmail.com>
2016-01-17 19:30:16 +0100

    Video initialization

--- src/class_gfx_engine.cpp
+++ src/class_gfx_engine.cpp
@@ -2,12 +2,22 @@
 
 class_gfx_engine::class_gfx_engine(class_engine *m_parent): parent(m_parent)
 {
-
+	if (SDL_InitSubSystem(SDL_INIT_VIDEO) != 0)
+	{
+		printf("SDL_INIT_VIDEO failed: %s\n", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+	if ((window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0)) == NULL)
+	{
+		printf("SDL_CreateWindow failed: %s\n", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
 }
 
 class_gfx_engine::~class_gfx_engine(void)
 {
-
+	SDL_DestroyWindow(window);
+	SDL_QuitSubSystem(SDL_INIT_VIDEO);
 }
 
 void class_gfx_engine::frame(void)
