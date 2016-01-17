9703eeb7f9ff62a5a73f108ce2241630286aa763
Paul Delobbel <paul.delobbel@gmail.com>
2016-01-17 19:35:21 +0100

    "title"

--- inc/class_gfx_engine.h
+++ inc/class_gfx_engine.h
@@ -8,11 +8,11 @@ class class_gfx_engine
 		SDL_Window *window;
 
 	protected:
-		class_engine *parent;
+		class_game *parent;
 		void process_event(class_engine_event &e);
-		
+
 	public:
-		class_gfx_engine(class_engine *m_parent);
+		class_gfx_engine(class_game *m_parent);
 		~class_gfx_engine(void);
 
 		void frame(void);
--- src/class_gfx_engine.cpp
+++ src/class_gfx_engine.cpp
@@ -1,13 +1,13 @@
 #include "class_gfx_engine.h"
 
-class_gfx_engine::class_gfx_engine(class_engine *m_parent): parent(m_parent)
+class_gfx_engine::class_gfx_engine(class_game *m_parent): parent(m_parent)
 {
 	if (SDL_InitSubSystem(SDL_INIT_VIDEO) != 0)
 	{
 		printf("SDL_INIT_VIDEO failed: %s\n", SDL_GetError());
 		exit(EXIT_FAILURE);
 	}
-	if ((window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0)) == NULL)
+	if ((window = SDL_CreateWindow("title", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0)) == NULL)
 	{
 		printf("SDL_CreateWindow failed: %s\n", SDL_GetError());
 		exit(EXIT_FAILURE);
@@ -27,5 +27,5 @@ void class_gfx_engine::frame(void)
 
 void class_gfx_engine::process_event(class_engine_event &e)
 {
-	
+
 }
