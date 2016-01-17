a2b10615f5c92c69b3d7210db20bcc5c8c7552da
Paul Delobbel <paul.delobbel@gmail.com>
2016-01-17 17:20:41 +0100

    ok ok

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 
 int		main(int argc, char *argv[])
 {
-	cout << "Salut" << endl;
 	if (argc && argv)
 		std::cout << "Avoid flags." << std::endl;
-    std::cout<<"lolo"<<std::endl;
 	return (EXIT_SUCCESS);
 }
